bccb09943a7ae850b270929df62d75c31c8e9af0
Gary van der Merwe <garyvdm@gmail.com>
2015-07-23 10:39:32 +0200

    Implement getpoints.

--- setup.py
+++ setup.py
@@ -8,9 +8,14 @@ setuptools.setup(
     install_requires=[
         'requests',
         'beautifulsoup4',
+        'slimit',  # for parsing js to an ast.
+        'python-dateutil',
+        'pytz',
+        'aniso8601',
     ],
     scripts=[
         'getriders',
+        'getpoints',
     ]
 )
 
